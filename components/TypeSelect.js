ea089c489818046ab0993269a29a9a155a4f851e
derickdecesare <deerriicckk@gmail.com>
2022-12-30 16:37:51 -0800

    changed formatting

--- components/TypeSelect.js
+++ components/TypeSelect.js
@@ -1,28 +1,32 @@
-import React from 'react'
+import React from "react";
 
-export default function TypeSelect({type, setType, setChatHistory, setMessage, setResponse}) {
-  console.log(type)
- 
+export default function TypeSelect({
+  type,
+  setType,
+  setChatHistory,
+  setMessage,
+  setResponse,
+}) {
+  console.log(type);
 
   function setTypeRefresh(e) {
-    setType(e.target.value)
-    setChatHistory('')
-    setMessage('')
-    setResponse('')
-    
+    setType(e.target.value);
+    setChatHistory("");
+    setMessage("");
+    setResponse("");
   }
 
   return (
-    <div className=' flex flex-row justify-center' >
-      <label htmlFor="site" className="block text-sm font-medium text-gray-900">
-        
-      </label>
+    <div className=" flex flex-row justify-center">
+      <label
+        htmlFor="site"
+        className="block text-sm font-medium text-gray-900"
+      ></label>
       <select
         id="site"
         name="site"
-        className=' block sm:w-half outline outline-gray-500 w-half rounded-md focus:outline-indigo-500 py-2 pl-3 pr-10 text-base focus:border-blue-600 focus:ring-indigo-500 sm:text-sm'
+        className=" block w-half rounded-md border border-2 border-blue-500  py-2 pl-3 pr-10 text-base focus:border-blue-600 focus:ring-indigo-500 sm:text-sm"
         value={type}
-        
         onChange={setTypeRefresh}
       >
         <option>Derick</option>
@@ -37,9 +41,7 @@ export default function TypeSelect({type, setType, setChatHistory, setMessage, s
         <option>Nick</option>
         <option>Alec</option>
         <option>Nicole</option>
-        
-        
       </select>
     </div>
-  )
+  );
 }
