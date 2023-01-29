289faf614a8f72a1311b9f07413e055abb27c8fc
derickdecesare <deerriicckk@gmail.com>
2023-01-29 13:02:26 -0800

    formating good

---
+++ components/HeaderTypeSelect.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+export default function HeaderTypeSelect({ type, setType }) {
+  console.log(type);
+
+  function setTypeRefresh(e) {
+    setType(e.target.value);
+  }
+
+  return (
+    <div className="flex">
+      <select
+        id="site"
+        name="site"
+        className="block w-1/2 rounded-md outline-none border-2 border-blue-400 ml-2 mr-12 my-1   text-white  text-base  bg-gray-900 sm:text-md"
+        value={type}
+        onChange={setTypeRefresh}
+      >
+        <option>ISTJ</option>
+        <option>ISFJ</option>
+        <option>INFJ</option>
+        <option>INTJ</option>
+        <option>ISTP</option>
+        <option>INTP</option>
+        <option>ISFP</option>
+        <option>INFP</option>
+        <option>ESTP</option>
+        <option>ESFP</option>
+        <option>ENFP</option>
+        <option>ENTP</option>
+        <option>ESTJ</option>
+        <option>ESFJ</option>
+        <option>ENFJ</option>
+        <option>ENTJ</option>
+      </select>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { Fragment, useState, useRef, useEffect } from "react";
-import TypeSelect from "../components/TypeSelect";
+import HeaderTypeSelect from "../components/HeaderTypeSelect";
 import { AiFillAccountBook } from "react-icons/ai";
 import { BiSend, BiRightArrow } from "react-icons/bi";
 import { BsPlusCircle, BsArrowRightShort } from "react-icons/bs";
@@ -104,24 +104,30 @@ export default function Chat() {
         <meta name="msapplication-TileColor" content="#2932dd" />
         <meta name="theme-color" content="#ffffff"></meta>
       </Head>
-      <div className="flex bg-gray-500 text-white absolute top-0 bottom-0 left-0 right-0">
+      <header className="bg-gray-900 fixed top-0 right-0 left-0 py-1 text-white z-10 lg:hidden flex flex-row align-middle justify-between">
+        <HeaderTypeSelect type={type} setType={setType} />
+        <div className="m-2 text-4xl  bg-gray-600 rounded-full hover:bg-gray-900  cursor-pointer text-black">
+          <BsPlusCircle onClick={clearChat} />
+        </div>
+      </header>
+      <div className="flex bg-gray-500 text-white absolute top-14 lg:top-0 bottom-0 left-0 right-0">
         <SideBar type={type} setType={setType} />
-        <div className="absolute top-0 right-0 m-4 text-4xl  bg-gray-600 rounded-full hover:bg-gray-900  cursor-pointer text-black">
+        <div className="absolute top-0 right-0 m-4 text-4xl  bg-gray-600 rounded-full hover:bg-gray-900 hidden lg:block cursor-pointer text-black">
           <BsPlusCircle onClick={clearChat} />
         </div>
 
-        <section className="flex-1 w-full overflow-y-auto lg:pl-64 bg-gray-500 pb-32">
+        <section className="flex-1 w-full overflow-y-auto lg:pl-64 bg-gray-500 pb-64">
           <div className="text-left w-full">
             {chatLog.map((message, index) => (
               <ChatMessage message={message} key={index} dummy={dummy} />
             ))}
           </div>
           <div ref={dummy}></div>
-          <div className=" fixed bottom-0 lg:left-72 left-0 right-0  py-2 px-12 ">
+          <div className=" fixed bottom-0 lg:left-72 left-0 right-0 pr-5 pl-3 ">
             <form onSubmit={handleSubmit}>
-              <div className="relative bg-gray-800 rounded-lg mb-6 mt-12 shadow">
+              <div className="relative bg-gray-800 rounded-lg mb-8 mt-12 shadow">
                 <input
-                  className="bg-gray-800 w-11/12  border-none rounded-lg outline-none shadow p-6 text-white text-xl"
+                  className="bg-gray-800 w-11/12  border-none rounded-lg outline-none shadow p-4 text-white text-xl"
                   rows="1"
                   value={input}
                   onChange={(e) => setInput(e.target.value)}
@@ -148,12 +154,12 @@ const ChatMessage = ({ message, dummy }) => {
   return (
     <>
       <div
-        className={`flex w-full mx-auto py-4 pl-8 pr-2 bg-gray-500 justify-center ${
+        className={`flex w-full mx-auto py-3  bg-gray-500 justify-center ${
           message.user === "gpt" && "bg-gray-700"
         }`}
       >
         <div className="flex w-full lg:px-12 py-4  pl-4 pr-12  gap-4 justify-between text-left items-start">
-          <div className="flex gap-8 items-start">
+          <div className="flex gap-4 items-start">
             <div className={`rounded-full w-11 h-11 shrink-0 items-start`}>
               {message.user === "gpt" ? (
                 <img
@@ -172,7 +178,7 @@ const ChatMessage = ({ message, dummy }) => {
                 // />
               )}
             </div>
-            <div className="text-xl text-left leading-loose">
+            <div className="text-xl text-left leading-loose text-gray-100">
               {message.message}
             </div>
           </div>
